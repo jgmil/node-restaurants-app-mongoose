fd448c50d4058744b28eae7c5910ad7e4881c873
Julia Miller <opie.julia@gmail.com>
2018-04-08 21:58:36 -0400

    updated CRUD

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/restaurants-app';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-restaurants-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blogPostSeedData';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blogPostSeedData';
 exports.PORT = process.env.PORT || 8080;
